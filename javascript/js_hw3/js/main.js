7ce06fdd4aa40e09d056471a841f71af94038b1d
MahireAsadzade <56446463+MahireAsadzade@users.noreply.github.com>
2019-11-03 16:13:53 +0400

    Update main.js

--- javascript/js_hw3/js/main.js
+++ javascript/js_hw3/js/main.js
@@ -1,17 +1,4 @@
-function calculator(){
-
-    let  num1= +prompt("Enter new num1: ");
-    let  num2= +prompt("Enter new num2: ");
-      while(num1 ==="" || num1 === null || isNaN(num1) || isNaN(num2) || num2 ==="" || num2 === null ){
-         num1 = +prompt("Invalid Value! Please, enter num1 again: ", `${num1}`);
-         num2 = +prompt("Invalid Value! Please, enter num2 again: ", `${num2}`);
-      }
-  
-      let operation= prompt("Enter mathematical operation:(+,-,*,/): ");
-      while( operation != "+" && operation != "-" && operation != "*" && operation != "/" ){
-          operation= prompt("Invalid Value! Please, enter new mathematical operation:(+,-,*,/): ");
-      }
-
+function calculator(num1, num2, operation){
     switch(operation){
         case "+":
            return num1+num2; break;
@@ -24,4 +11,16 @@ function calculator(){
     }
 }
 
-console.log(calculator());
+let  num1= +prompt("Enter new num1: ");
+let  num2= +prompt("Enter new num2: ");
+  while(num1 ==="" || num1 === null || isNaN(num1) || isNaN(num2) || num2 ==="" || num2 === null ){
+     num1 = +prompt("Invalid Value! Please, enter num1 again: ", `${num1}`);
+     num2 = +prompt("Invalid Value! Please, enter num2 again: ", `${num2}`);
+  }
+
+  let operation= prompt("Enter mathematical operation:(+,-,*,/): ");
+  while( operation != "+" && operation != "-" && operation != "*" && operation != "/" ){
+      operation= prompt("Invalid Value! Please, enter new mathematical operation:(+,-,*,/): ");
+  }
+
+console.log(calculator(num1, num2, operation));
