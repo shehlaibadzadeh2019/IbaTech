52e14f0b7b2e22fd9a9b8bd59a35dbb5c65c1cab
MahireAsadzade <56446463+MahireAsadzade@users.noreply.github.com>
2019-11-06 20:01:59 +0400

    Add files via upload

---
+++ javascript/js_hw8/js/main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let input = document.createElement("input");
+    input.setAttribute("type", "number");
+    input.setAttribute("placeholder", "Price");
+    document.body.appendChild(input);
+
+    let span = document.createElement("span");
+    
+    let button = document.createElement("button");
+    button.innerHTML = `X`;
+
+    let phrase =  document.createElement("div");
+    phrase.innerHTML = "Please enter correct price!";
+
+    input.addEventListener("focusin", ()=>{
+        input.style.border = "2px solid green";
+        input.value = "";
+    });
+
+    input.addEventListener("focusout", ()=>{
+        input.style.border = "1px solid gray";
+        if( input.value > 0 ){
+            span.innerHTML = `Current price: ${input.value}`;
+            input.before(span);
+            span.after(button);
+            input.style.color = "green";
+     }
+        else{
+            input.after(phrase);
+            input.style.border = "2px solid red";
+            phrase.style.color= "red";
+     }
+    });
+
+    button.addEventListener("click",()=>{
+        span.remove();
+        button.remove();
+        input.value = 0;
+        
+    } );
+
+  });
