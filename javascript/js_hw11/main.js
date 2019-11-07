56cf7a67bb80dfc6b637f5cdae00fdf0b5baec98
MahireAsadzade <56446463+MahireAsadzade@users.noreply.github.com>
2019-11-07 22:44:05 +0400

    Update main.js

--- javascript/js_hw11/main.js
+++ javascript/js_hw11/main.js
@@ -1,12 +1,18 @@
-let key_arr = ["Enter", "KeyS", "KeyE", "KeyO", "KeyN", "KeyZ", "KeyL"];
-let btn_arr = [".btn_Ent", ".btn_S", ".btn_E", ".btn_O", ".btn_N", ".btn_Z" ,".btn_L"];
-
-for(let i=0; i < key_arr.length; i++){
+let obj ={
+    "Enter":".btn_Ent",
+    "KeyS": ".btn_S",
+    "KeyE": ".btn_E",
+    "KeyO": ".btn_O",
+    "KeyN": ".btn_N",
+    "KeyZ": ".btn_Z",
+    "KeyL": ".btn_L"
+};
+for(let property in obj){
     document.addEventListener('keydown',(event)=>{
-        if(event.code == key_arr[i]){
-            document.querySelector(btn_arr[i]).classList.add("active");
-            btn_arr.forEach((val)=>{
-                if(val != btn_arr[i])
+        if(event.code == property){
+            document.querySelector(obj[property]).classList.add("active");
+            Object.values(obj).forEach((val)=>{
+                if(val != obj[property])
                 document.querySelector(val).classList.remove("active");
             })
         }
