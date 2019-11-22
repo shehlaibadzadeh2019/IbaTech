492c64018da1a2dbfcc06943337e848042e5ef7b
MahireAsadzade <56446463+MahireAsadzade@users.noreply.github.com>
2019-11-22 19:07:36 +0400

    Delete main.js

--- Advanced_Html - Css/pizza/src/js/main.js
+++
@@ -1,7 +0,0 @@
-var burger = document.querySelector('.header-burger');
-var nav = document.querySelector('.header-nav');
-
-burger.addEventListener('click', function() {
-    burger.classList.toggle('active');
-    nav.classList.toggle('active');
-})
